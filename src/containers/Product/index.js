5c4b54fa48d225220d187b543d72c342afb2914d
Aarchi-Ayoub <ayoubaa084@gmail.com>
2022-07-01 13:47:12 +0100

    delete log

--- src/containers/Product/index.js
+++ src/containers/Product/index.js
@@ -4,7 +4,6 @@ import FastImage from 'react-native-fast-image';
 import styles from './styles';
 
 export default ({data}) => {
-  console.log(data);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{data?.title}</Text>
